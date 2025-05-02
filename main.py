2c962adbb6c62645a572b69ba07167b131b0b34b
Kaedyn Eastall (School) <eask23083@wainuiomatahigh.school.nz>
2025-05-02 14:43:01 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,81 @@
 '''
-STANDARD AS92004 Quiz About New Zealand
+Quiz about New Zealand
 Kaedyn Eastall
 Licensed with the JSS General Public License 001 - https://github.com/Jester-Software-Systems/JSS_LICENSE_DIRECTORY/blob/2025-Licenses/MARCH/JSS-GPL1
 '''
 import time
+import os
 
 # Lists & Variables
 int_score = 0
 int_question = 0
+str_name = ""
 LIST_QUESTIONS = [
-    "Who found New Zealand?",
+    "What british sailor officially mapped New Zealand?",
     "What is the capital of New Zealand?",
     "Where is the big carrot?",
-    "What does \"Kia Ora\" mean?",
     "What is the name of the glowworm caves on the North Island?",
     "What is the name of this 328m high landmark in Auckland?",
     "Can you name the highest mountain in New Zealand?",
-    "(True or False) The South Island the largest island in the world?"
+    "(True or False) The South Island the largest island in the world?",
+    "What does the māori word \"Taraka\" mean?"
 ]
 LIST_ANSWERS = [
-    "Abel Tasman",
+    "James Cook",
     "Wellington",
     "Ohakune",
-    "Hello",
     "Waitomo",
     "Sky Tower",
     "Mount Cook",
-    "False"
+    "False",
+    "Truck"
 ]
-LIST_YAP = [
-    "Welcome to Kaedyn's quiz! Let's go."
+LIST_CHAT = [
+    "Welcome to Kaedyn's quiz! Whats your name?",
+    "Ok! Please wait three seconds and then the quiz will restart.",
+    "Alright, thanks for playing!"
 ]
 
-# Main Question Asking
-print(LIST_YAP[0])
-for i in range (len(LIST_QUESTIONS)):
+# Intro & Question Print Loop
+print(LIST_CHAT[0])
+str_name = input()
+def playing():
+    global int_score
+    for i in range (len(LIST_QUESTIONS)):
+        time.sleep(1)
+        print("\n")
+        print(f"{LIST_QUESTIONS[i]}")
+        ans = input()
+        ans = ans.lower().title()
+        if ans == LIST_ANSWERS[i]:
+            int_score += 5
+            print(f"✅  That's right! You now have {int_score} points.")
+            i =+ 1
+        else: 
+            int_score -= 2.5
+            print(f"❌  That's wrong. The answer was {LIST_ANSWERS[i]}.\nYou now have {int_score} points.")
+            i =+ 1
+
+def runit():
+    global int_score
     time.sleep(1)
     print("\n")
-    print(f"{LIST_QUESTIONS[i]}")
-    ans = input()
-    ans = ans.lower()
-    ans = ans.title()
-    if ans == LIST_ANSWERS[i]:
-        int_score += 5
-        print(f"That's right! You now have {int_score} points.")
-        i =+ 1
+    print(f"Alright, {str_name}. You finished with {int_score}/40 points! Want to try again?")
+    tryagain = input()
+    tryagain = tryagain.lower().title()
+    if tryagain == "Yes":
+        print(LIST_CHAT[1])
+        int_question = 0
+        int_score = 0
+        time.sleep(3)
+        os.clear()
+        playing()
+    elif tryagain == "No":
+        print(LIST_CHAT[2])
+        quit()
     else:
-        int_score -= 2.5
-        print(f"That's wrong. The answer was {LIST_ANSWERS[i]}.\nYou now have {int_score} points.")
-        i =+ 1
+        print(f"Hey! {str_name}! It's a yes or no question!")
+        time.sleep(1)
+        runit()
+playing()
+runit()
