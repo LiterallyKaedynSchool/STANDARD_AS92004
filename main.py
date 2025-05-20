04210b0a13958a15669727775ee13c41738d19b2
Kaedyn Eastall (School) <eask23083@wainuiomatahigh.school.nz>
2025-05-20 20:41:36 +1200

    Update main.py

--- main.py
+++ main.py
@@ -11,41 +11,44 @@ int_score = 0 # Setting the 'int_score' variable to 0
 int_question = 0 # Setting the user on question 0
 str_name = "" # Setting the user name to blank before they input it.
 LIST_QUESTIONS = [ # A list with all the questions
-    "What british sailor officially mapped New Zealand?",
-    "What is the capital of New Zealand?",
-    "Where is the big carrot?",
-    "What is the name of the glowworm caves on the North Island?",
-    "What is the name of this 328m high landmark in Auckland?",
-    "Can you name the highest mountain in New Zealand?",
-    "(True or False) The South Island the largest island in the world?",
-    "What does the māori word \"Taraka\" mean?"
+    "What british sailor officially mapped New Zealand?\nA. James Cook\nB. Ernest Rutherford\nC. Dame Whina Cooper\nD. Richard Seddon",
+    "What is the capital of New Zealand?\nA. Auckland\nB. Wellington\nC. Christchurch\nD. Taupō",
+    "Where is the big carrot?\nA. Ohakune\nB. Masterton\nC. Foxton\nD. Waiouru",
+    "What is the name of the glowworm caves on the North Island?\nA. Waitomo\nB. Pukeatua\nC. Tihoi\nD. Kaeo",
+    "What is the name of this 328m high landmark in Auckland?\nA. Eiffel Tower\nB. Sky Tower\nC. Big Ben\nD. Tower",
+    "Can you name the highest mountain in New Zealand?\nA. Mount Ruapehu\nB. Mount Tasman\nC. Mount Cook\nD. Mount Tongariro",
+    "(True or False) The South Island the largest island in the world?\nA. True\nB. False",
+    "What does the māori word \"Taraka\" mean?\nA. Car\nB. Airplane\nC. Bird\nD. Truck"
 ]
 LIST_ANSWERS = [ # Alist with all the answers
-    "James Cook",
-    "Wellington",
-    "Ohakune",
-    "Waitomo",
-    "Sky Tower",
-    "Mount Cook",
-    "False",
-    "Truck"
+    "A",
+    "B",
+    "A",
+    "A",
+    "B",
+    "C",
+    "B",
+    "D"
 ]
 LIST_CHAT = [ # A list with all the other things I have to say.
     "Welcome to Kaedyn's quiz! Whats your name?",
     "Ok! Please wait three seconds and then the quiz will restart.",
-    "Alright, thanks for playing!"
+    "Alright, thanks for playing!",
+    "Ensure you input A, B, C, or D for your answers."
 ]
 
 # Intro & Question Print Loop
 print(LIST_CHAT[0])
-str_name = input() # User name input
+str_name = input("Name: ") # User name input
+time.sleep(0.75)
+print(LIST_CHAT[3])
 def playing():
     global int_score
     for i in range (len(LIST_QUESTIONS)):
         time.sleep(1)
         print("\n")
-        print(f"{LIST_QUESTIONS[i]}")
-        ans = input()
+        print(f"{LIST_QUESTIONS[i]}\n")
+        ans = input("Answer: ")
         ans = ans.lower().title()
         if ans == LIST_ANSWERS[i]:
             int_score += 5
@@ -60,8 +63,12 @@ def runit():
     global int_score
     time.sleep(1)
     print("\n")
-    print(f"Alright, {str_name}. You finished with {int_score}/40 points! Want to try again?")
-    tryagain = input()
+    if int_score > 20: # Check user score
+        print(f"Alright, {str_name}. You passed my quiz with {int_score} points! ") # Passed the quiz
+    else:
+        print(f"Nice try, {str_name}! You failed my quiz with {int_score} points\nYou need at least 20 points to pass.") # Failed the quiz
+
+    tryagain = input("Would you like to try again? (Yes or no): ")
     tryagain = tryagain.lower().title()
     if tryagain == "Yes":
         print(LIST_CHAT[1])
